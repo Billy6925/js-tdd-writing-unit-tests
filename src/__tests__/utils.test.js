aa821e1e3095b870c5896a9e329d6e3e1a7b69c9
Billy Eskein <billyeskein@gmail.com>
2025-02-08 17:42:30 +0300

    Update src/utils.js

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,13 @@
 // Your tests here
+import {pointsForWord} from '../utils';
+
+describe('pointsForWord', () => {
+    it('calcultes the total points for a word(1 point per vowel, 2 points per consonant)', () => {
+        // Arrange
+        const word = "test";
+        // Act
+        const points = pointsForWord(word);
+        //Assert
+        expect(points).toBe(7);
+    });
+});
